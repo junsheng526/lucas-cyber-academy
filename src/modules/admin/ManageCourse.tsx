28de2e97df8cffa51bff6edf350b9ee0bc17bd00
junsheng <junshengchoo57@gmail.com>
2025-01-31 20:15:11 +0800

    fix: fixed update images issues

--- src/modules/admin/ManageCourse.tsx
+++ src/modules/admin/ManageCourse.tsx
@@ -24,22 +24,8 @@ import { useAuth } from "../../firebase/useAuth";
 import { useFileUpload } from "../../supabase/useFileUpload";
 import { DeleteConfirmationDialog } from "../../components/templates/DeleteCourseForm";
 import { useCourses } from "../../hooks/useCourses";
-import { coursesListing } from "../../data/jsons/coursesListing";
 
 const ManageCourses = () => {
-  // useEffect(() => {
-  //   let data = coursesListing;
-  //   const insert = async () => {
-  //     for (let i = 0; i < data.length; i++) {
-  //       console.log("Inserting data -> " + data[i].title);
-  //       const newCourseId = await firestoreService.insertDoc(
-  //         Docs.COURSES,
-  //         data[i]
-  //       );
-  //     }
-  //   };
-  //   insert();
-  // }, []);
   const { courses, loading, error, setCourses } = useCourses();
   const [open, setOpen] = useState(false);
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
@@ -76,40 +62,19 @@ const ManageCourses = () => {
     { field: "id", headerName: "ID", flex: 1 },
     { field: "title", headerName: "Course Name", flex: 1.5 },
     { field: "description", headerName: "Description", flex: 2 },
-    { field: "duration", headerName: "Duration", flex: 1 },
-    { field: "category", headerName: "Category", flex: 1 },
-    { field: "price", headerName: "Price", flex: 1 },
-    { field: "lessons", headerName: "Lessons", flex: 1 },
     { field: "level", headerName: "Level", flex: 1 },
-    { field: "isAds", headerName: "Is Ads", flex: 1 },
-    { field: "createdAt", headerName: "Created At", flex: 1.5 },
-    { field: "modifiedAt", headerName: "Modified At", flex: 1.5 },
-    { field: "href", headerName: "Course Link", flex: 1 },
     {
       field: "featuredImage",
       headerName: "Featured Image",
       flex: 2,
       renderCell: (params) => {
-        const fileName = params.value?.split("/").pop(); // Extract file name from the URL
-        return <span>{fileName}</span>; // Display the file name
+        return <img src={params.value} alt="Featured" />;
       },
     },
-    {
-      field: "galleryImgs",
-      headerName: "Gallery Image",
-      flex: 2,
-      renderCell: (params) => {
-        const galleryFileNames = params.value?.map((url: string) =>
-          url.split("/").pop()
-        ); // Extract file names from the gallery image URLs
-        return galleryFileNames ? galleryFileNames.join(", ") : "";
-      },
-    },
-    { field: "tags", headerName: "Tags", flex: 1 },
     {
       field: "actions",
       headerName: "Actions",
-      flex: 1,
+      flex: 2,
       renderCell: (params) => (
         <Box>
           <Button
@@ -191,6 +156,8 @@ const ManageCourses = () => {
   };
 
   const handleClickOpen = () => {
+    setCurrentFeaturedImageUrl(null);
+    setCurrentGalleryImagesUrl([]);
     setOpen(true);
     resetForm();
   };
@@ -240,20 +207,16 @@ const ManageCourses = () => {
   const handleFileUpload = async (file: File): Promise<string> => {
     const allowedTypes = ["image/png", "image/jpeg", "image/jpg"];
 
-    // Validate file type
     if (!allowedTypes.includes(file.type)) {
       throw new Error("Only PNG, JPG, and JPEG files are allowed.");
     }
 
-    // Upload the file using the uploadFiles function
     const uploadedUrls = await uploadFiles([file], user?.uid);
 
-    // If the upload fails (uploadedUrls is null), throw an error
     if (!uploadedUrls) {
       throw new Error("Failed to upload the file.");
     }
 
-    // Return the first URL since it's a single file upload
     return uploadedUrls[0];
   };
 
@@ -290,6 +253,49 @@ const ManageCourses = () => {
     }
   };
 
+  // const handleUpdateCourse = async () => {
+  //   try {
+  //     if (featuredImageFile) {
+  //       const imageUrl = await handleFileUpload(featuredImageFile);
+  //       newCourse.featuredImage = imageUrl;
+  //     }
+
+  //     if (galleryImageFiles) {
+  //       const galleryUrls = await Promise.all(
+  //         Array.from(galleryImageFiles).map((file, index) =>
+  //           handleFileUpload(file)
+  //         )
+  //       );
+  //       newCourse.galleryImgs = [...galleryUrls]; // Combine with existing images
+  //     }
+
+  //     // If no new images were uploaded, ensure the course fields reflect null or empty arrays
+  //     if (!featuredImageFile && !currentFeaturedImageUrl) {
+  //       newCourse.featuredImage = ""; // Set featured image to null if removed
+  //     }
+
+  //     if (!galleryImageFiles && currentGalleryImagesUrl.length === 0) {
+  //       newCourse.galleryImgs = []; // Set gallery images to empty array if removed
+  //     }
+
+  //     // Update the course data in Firestore
+  //     const { id, ...courseData } = newCourse;
+  //     await firestoreService.updateDoc(Docs.COURSES, id as string, courseData);
+
+  //     setCourses((prevCourses) =>
+  //       prevCourses.map((course) =>
+  //         course.id === id ? { ...course, ...courseData } : course
+  //       )
+  //     );
+
+  //     // Reset and close modal after updating
+  //     setOpen(false);
+  //     resetForm();
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
   const handleUpdateCourse = async () => {
     try {
       if (featuredImageFile) {
@@ -299,29 +305,13 @@ const ManageCourses = () => {
 
       if (galleryImageFiles) {
         const galleryUrls = await Promise.all(
-          Array.from(galleryImageFiles).map((file, index) =>
-            handleFileUpload(file)
-          )
+          Array.from(galleryImageFiles).map((file) => handleFileUpload(file))
         );
-        newCourse.galleryImgs = [...galleryUrls]; // Combine with existing images
+        newCourse.galleryImgs = galleryUrls;
       }
 
-      // If no new images were uploaded, ensure the course fields reflect null or empty arrays
-      if (!featuredImageFile && !currentFeaturedImageUrl) {
-        newCourse.featuredImage = ""; // Set featured image to null if removed
-      }
-
-      if (!galleryImageFiles && currentGalleryImagesUrl.length === 0) {
-        newCourse.galleryImgs = []; // Set gallery images to empty array if removed
-      }
-
-      // Update the course data in Firestore
       const { id, ...courseData } = newCourse;
       await firestoreService.updateDoc(Docs.COURSES, id as string, courseData);
-
-      // since already upload to firebase, clear the local uploaded files state
-      // the local url
-      setGalleryImageFiles([]);
 
       setCourses((prevCourses) =>
         prevCourses.map((course) =>
@@ -329,32 +319,11 @@ const ManageCourses = () => {
         )
       );
 
-      // Reset and close modal after updating
       setOpen(false);
       resetForm();
     } catch (err) {
-      console.error(err);
+      console.error("Error updating course:", err);
     }
-  };
-
-  const handleRemoveFeaturedImage = () => {
-    setCurrentFeaturedImageUrl(null);
-    setFeaturedImageFile(null);
-    setNewCourse((prevCourse) => ({
-      ...prevCourse,
-      featuredImage: "",
-    }));
-  };
-
-  const handleRemoveGalleryImage = (index: number) => {
-    const updatedGalleryImages = [...currentGalleryImagesUrl];
-    updatedGalleryImages.splice(index, 1);
-    // Update the preview state
-    setCurrentGalleryImagesUrl(updatedGalleryImages);
-    setNewCourse((prevCourse) => ({
-      ...prevCourse,
-      galleryImgs: updatedGalleryImages,
-    }));
   };
 
   const handleFeaturedImageChange = (
@@ -362,45 +331,31 @@ const ManageCourses = () => {
   ) => {
     const file = e.target.files?.[0];
     if (file) {
-      // Preview the file locally
       const previewUrl = URL.createObjectURL(file);
 
-      // If there's already a featured image, keep the old one and add the new one
-      if (currentFeaturedImageUrl) {
-        // Combine the previous image with the new one
-        setFeaturedImageFile(file);
-      } else {
-        // Otherwise, just set the new image
-        setFeaturedImageFile(file);
-      }
-
-      setCurrentFeaturedImageUrl(previewUrl); // Update state with the preview URL
+      setFeaturedImageFile(file);
+      setCurrentFeaturedImageUrl(previewUrl);
     }
   };
 
-  // Gallery Image Handler
   const handleGalleryImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
     if (files) {
       const newFilesArray = Array.from(files); // Convert FileList to array
-      const totalFilesCount =
-        currentGalleryImagesUrl.length + newFilesArray.length;
 
-      if (totalFilesCount > 9) {
-        alert("You cannot upload more than 9 gallery images.");
+      if (newFilesArray.length > 6) {
+        alert("You cannot upload more than 6 gallery images.");
         return;
       }
 
-      // Combine new gallery images with existing ones
+      // Generate preview URLs for the new images
       const newFileUrls = newFilesArray.map((file) =>
         URL.createObjectURL(file)
       );
-      setGalleryImageFiles((prevFiles) => [...prevFiles, ...newFilesArray]);
 
-      setCurrentGalleryImagesUrl((prevImages) => [
-        ...prevImages,
-        ...newFileUrls,
-      ]);
+      // Overwrite existing images with new ones
+      setGalleryImageFiles(newFilesArray);
+      setCurrentGalleryImagesUrl(newFileUrls);
     }
   };
 
@@ -469,22 +424,6 @@ const ManageCourses = () => {
                       marginRight: "8px",
                     }}
                   />
-                  <button
-                    type="button"
-                    onClick={handleRemoveFeaturedImage}
-                    style={{
-                      backgroundColor: "#FF6347",
-                      border: "none",
-                      color: "white",
-                      padding: "8px 16px",
-                      cursor: "pointer",
-                      borderRadius: "4px",
-                      fontSize: "14px",
-                      transition: "background-color 0.3s",
-                    }}
-                  >
-                    Remove Featured Image
-                  </button>
                 </div>
               )}
               <input
@@ -522,26 +461,6 @@ const ManageCourses = () => {
                           borderRadius: "8px",
                         }}
                       />
-                      <button
-                        type="button"
-                        onClick={() => handleRemoveGalleryImage(index)}
-                        style={{
-                          position: "absolute",
-                          top: "8px",
-                          right: "8px",
-                          backgroundColor: "rgba(255, 99, 71, 0.8)",
-                          border: "none",
-                          color: "white",
-                          padding: "4px 8px",
-                          cursor: "pointer",
-                          borderRadius: "4px",
-                          fontSize: "12px",
-                          opacity: "0.9",
-                          transition: "opacity 0.3s",
-                        }}
-                      >
-                        Remove
-                      </button>
                     </div>
                   ))}
                 </div>
