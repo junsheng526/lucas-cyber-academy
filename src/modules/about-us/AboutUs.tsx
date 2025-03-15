1481d0cb104584fcaab2ceb94882ae60b310e9ba
DingHeng <jayleedingheng0401@gmail.com>
2025-03-15 09:32:03 +0800

    feat: added About Us page

---
+++ src/modules/about-us/AboutUs.tsx
@@ -0,0 +1,35 @@
+import BackgroundSection from "../../components/atoms/background/BackgroundSection";
+import BgGlassmorphism from "../../components/atoms/background/BgGlassmorphism";
+import SectionHero from "../home/SectionHero";
+import SectionLecturer from "./SectionLecturer";
+import SectionClientSay from "./SectionClientSay";
+import SectionStatistic from "./SectionStatistic";
+import Layout from "../../components/templates/layout/Layout";
+
+const AboutUs: React.FC = () => {
+  return (
+    <Layout>
+      <div className="nc-PageAbout overflow-hidden relative px-[7rem]">
+        {/* ======== BG GLASS ======== */}
+        <BgGlassmorphism />
+
+        <div className="container space-y-16 lg:space-y-28">
+          <SectionHero
+            homeContentId="OWzezYNeCRH9YJMb8rPS"
+            className="text-start pt-10 pt-16 md:pb-16"
+          />
+
+          <SectionLecturer />
+          <div className="relative py-16">
+            <BackgroundSection />
+            <SectionClientSay />
+          </div>
+
+          <SectionStatistic />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default AboutUs;
